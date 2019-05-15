448923e2c4355cf87ab625add82fddd9d388077e
jiye-ML <woxinxie1234@163.com>
2019-05-15 08:42:25 +0800

    代码上传

---
+++ src/linear/__init__.py
@@ -0,0 +1,14 @@
+from src.linear.bayesian_regression import BayesianRegression
+from src.linear.emprical_bayes_regression import EmpiricalBayesRegression
+from src.linear.linear_regression import LinearRegression
+from src.linear.ridge_regression import RidgeRegression
+from src.linear.variational_linear_regression import VariationalLinearRegression
+
+
+__all__ = [
+    "BayesianRegression",
+    "EmpiricalBayesRegression",
+    "LinearRegression",
+    "RidgeRegression",
+    "VariationalLinearRegression"
+]
---
+++ src/preprocess/__init__.py
@@ -0,0 +1,11 @@
+from src.preprocess.gaussian import GaussianFeature
+from src.preprocess.label_transformer import LabelTransformer
+from src.preprocess.polynomial import PolynomialFeature
+from src.preprocess.sigmoidal import SigmoidalFeature
+
+__all__ = [
+    "GaussianFeature",
+    "LabelTransformer",
+    "PolynomialFeature",
+    "SigmoidalFeature"
+]
